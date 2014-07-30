f68edf078f50f8a96df61de9be1d1af205bedc22
Olivier Beaudoin <olivier@thoorium.com>
2014-07-30 18:46:16 -0400

    Moved camera stuff to Cameras, created cs files for each camera type

--- SharpDX.WPF/Cameras/BaseCamera.cs
+++ SharpDX.WPF/Cameras/BaseCamera.cs
@@ -5,7 +5,7 @@ using System.Windows.Input;
 
 // made after DXUTCamera.h & DXUTCamera.cpp
 
-namespace SharpDX.WPF
+namespace SharpDX.WPF.Cameras
 {
     /// <summary>
     /// Simple base camera class that moves and rotates.  The base class
@@ -529,74 +529,5 @@ namespace SharpDX.WPF
         private Matrix m_viewMat;
 
         #endregion Fields
-    }
-
-    /// <summary>
-    /// Simple first person camera class that moves and rotates.
-    /// It allows yaw and pitch but not roll.  It uses WM_KEYDOWN and
-    /// GetCursorPos() to respond to keyboard and mouse input and updates the
-    /// view matrix based on input.
-    /// </summary>
-    public partial class FirstPersonCamera : BaseCamera
-    {
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="eye"></param>
-        /// <param name="lookAt"></param>
-        /// <param name="vUp"></param>
-        public override void SetViewParams(Vector3 eye, Vector3 lookAt, Vector3 vUp)
-        {
-            base.SetViewParams(eye, lookAt, vUp);
-            m_viewRotQuat = Quaternion.Identity;
-        }
-    }
-
-    /// <summary>
-    /// Simple model viewing camera class that rotates around the object.
-    /// </summary>
-    public class ModelViewerCamera : BaseCamera
-    {
-        private Quaternion m_modelRotQuat;
-        private float m_radius;
-        private Matrix m_worldMat;
-
-        /// <summary>
-        ///
-        /// </summary>
-        public Matrix World { get { return m_worldMat; } }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="eye"></param>
-        /// <param name="lookAt"></param>
-        /// <param name="vUp"></param>
-        public override void SetViewParams(Vector3 eye, Vector3 lookAt, Vector3 vUp)
-        {
-            base.SetViewParams(eye, lookAt, vUp);
-
-            m_modelRotQuat = Quaternion.Identity;
-            m_radius = (eye - lookAt).Length();
-            UpdateWorld();
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="m_mouseQuat"></param>
-        protected override void MouseRotation(Quaternion m_mouseQuat)
-        {
-            m_modelRotQuat = m_modelRotQuat * m_mouseQuat;
-            UpdateWorld();
-        }
-
-        /// <summary>
-        ///
-        /// </summary>
-        private void UpdateWorld()
-        {
-            m_worldMat = Matrix.RotationQuaternion(m_modelRotQuat);
-        }
     }
 }
--- SharpDX.WPF/D3D.cs
+++ SharpDX.WPF/D3D.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpDX.WPF.Cameras;
+using System;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
